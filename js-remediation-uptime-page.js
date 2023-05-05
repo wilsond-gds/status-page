ada85c2952638c9fc1a60a664c1311628155a379
Derren Wilson <derren.wilson@digital.cabinet-office.gov.uk>
2023-05-05 10:37:53 +0100

    Updates to files after removing history widgets

--- js-remediation-uptime-page.js
+++ js-remediation-uptime-page.js
@@ -1,20 +1,20 @@
-// stop screenreaders reading out each SVG day image
+// stop screenreaders reading out each SVG day image if this is used
 const removeImageReadout = document.querySelectorAll('svg.day');
 removeImageReadout.forEach(item=>item.setAttribute('aria-hidden', 'true'));
-// add the word 'uptime' or 'no data' to the percentage counts on the Month headings
+// add the word 'uptime' or 'no data' to the percentage counts on the Month headings if needed
 const addCaptionToUptime = document.querySelectorAll('small.month-uptime');
 addCaptionToUptime.forEach(item=>{ const currentText = item.textContent;
     console.log(currentText);
     if(currentText == ""){item.textContent="No data"}else{ item.textContent = currentText + ' uptime';}
 
 })
-// Collapse the month headings into a single DOM node for screen readers
+// Collapse the month headings into a single DOM node for screen readers if needed
 const removeVarWrapping = document.querySelectorAll('var[data-var=year]')
 removeVarWrapping.forEach(item=>{
     const fullHeading = item.parentElement.textContent;
     item.parentElement.textContent = fullHeading;
     item.remove();
 })
-// Tell screen readers which is the selected tab
+// Tell screen readers which is the selected tab for 'inner' pages
 const reportSelectedTab = document.querySelector('a.current');
 reportSelectedTab.setAttribute('aria-selected','true')
